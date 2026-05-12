1d66a7421a4e16ec31187ea4e74d9572318971ef
周倩夙 <angela530228@gmail.com>
2026-05-12 16:45:09 +0800

    change

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/_number_guess_2/",
   plugins: [react(), tailwindcss()],
 });
